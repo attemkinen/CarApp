241780b05560d46ea09460258c1d0c3bd03363fe
attemkinen <atte.makinen1@outlook.com>
2023-04-07 15:45:41 +0300

    added Header to app & delete functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import Carlist from './components/carlist';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
+       <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          
+           
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            CarShop
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+    </Box>
       <Carlist/>
     </div>
   );
--- src/components/carlist.js
+++ src/components/carlist.js
@@ -3,33 +3,56 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
 import { AgGridReact } from "ag-grid-react";
 
+
+
+
+
 function Carlist () {
+  
 
     const [cars, setCars] = useState([]);
+    useEffect(()=>fetchData(), []);
+
+    const deleteCar = (params) => {
+      fetch(params,{method: "DELETE"})
+      .then(res => fetchData())
+      .catch(err => console.error(err))
+    }
+  
+    const SimpleComp = (params) => <button onClick={()=> deleteCar(params.value)}>Delete</button>;
+
+    
+   
+
+    
 
     const [columnDefs] = useState([
-        {field: "brand", sortable: true, filter: true},
-        {field: "model", sortable: true, filter: true},
-        {field: "fuel", sortable: true, filter: true},
-        {field: "year", sortable: true, filter: true},
-        {field: "price", sortable: true, filter: true},
+        {field: "brand", sortable: true, filter: true, flex: 1},
+        {field: "model", sortable: true, filter: true, flex:1},
+        {field: "fuel", sortable: true, filter: true, flex:1},
+        {field: "year", sortable: true, filter: true,flex:1},
+        {field: "price", sortable: true, filter: true,flex:1},
+        {field: "_links.self.href", sortable: true, filter: true, cellRenderer: SimpleComp, flex:1},
 
         
     ])
 
-    useEffect(()=>{
+    const fetchData = ()=> {
         fetch("http://carrestapi.herokuapp.com/cars")
         .then(response => response.json())
         .then(data =>setCars(data._embedded.cars))
         .catch(err => console.error(err))
 
-    },[]);
+    }
+    
 
     return (
       <div className="ag-theme-material"
       style={{height: 600, width:"90%", margin: "auto"}}>
+        
 
         <AgGridReact
+       
         rowData={cars}
         columnDefs={columnDefs}>
           
@@ -38,5 +61,6 @@ function Carlist () {
       </div>
     );
 }
+
 
 export default Carlist;
